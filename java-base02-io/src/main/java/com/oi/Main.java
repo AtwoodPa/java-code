1b64bae741fe204607adbf01ce6749c5219e6b67
Karl <supanpan199919@163.com>
2024-07-11 10:07:52 +0800

    NewFeature: Java语言基础面试题模块搭建

---
+++ java-base02-io/src/main/java/com/oi/Main.java
@@ -0,0 +1,11 @@
+package com.oi;
+
+/**
+ * @author supanpan
+ * @date 2024/07/11
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
