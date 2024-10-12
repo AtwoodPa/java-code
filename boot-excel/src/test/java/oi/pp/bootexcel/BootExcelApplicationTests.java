d20c80d5c4e7954419b6249ebdf133bdc94c46cf
AtwoodPa <110728122+AtwoodPa@users.noreply.github.com>
2024-10-12 15:52:32 +0800

    Feature
    boot-excel

---
+++ boot-excel/src/test/java/oi/pp/bootexcel/BootExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package oi.pp.bootexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
